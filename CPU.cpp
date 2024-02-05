28a4a2bacca53d54a2d46f09d9b9156850f73aec
PyDOS8 <124309800+PyDOS8@users.noreply.github.com>
2024-02-05 08:07:26 -0500

    Create CPU.cpp

---
+++ CPU.cpp
@@ -0,0 +1,76 @@
+#include <stdio.h>
+#include <string>
+extern "C"{
+    struct RCS{
+        std::string*memory[3];
+        void writeToMemory(std::string *item){
+            for(int i = 0; i < 3; i++){
+                if(*memory[i] == ""){
+                    *memory[i] = *item;
+                }else{
+                    *memory[i] = "";
+                    *memory[i] = *item;
+                }
+            }
+        }
+        void clearMemory(){
+            for(int i = 0; i < 3; i++){
+                if(*memory[i] != ""){
+                    *memory[i] = "";
+                }else{
+                    printf("Cannot clear the memory because it is already cleared!\n");
+                }
+            }
+        }
+        void deallocateStringMemory(){
+            for(int i = 0; i < 4; i++){
+                memory[i] = nullptr;
+                delete[] memory;
+            }
+        }
+    };
+    struct RID{
+        double *doubleMemory[3];
+        int *intMemory[3];
+        void writeToDoubleMemory(int index, double value){
+            if(*doubleMemory[index] == 0){
+                *doubleMemory[index] = value;
+            }else{
+                *doubleMemory[index] = 0;
+                *doubleMemory[index] = value;
+            }
+        }
+        void clearDoubleMemory(int index){
+            if(*doubleMemory[index] > 0){
+                *doubleMemory[index] = 0;
+            }else{
+                printf("Cannot clear memory because it is already empty!\n");
+            }
+        }
+        void writeToIntMemory(int index, int value){
+            if(*intMemory[index] == 0){
+                *intMemory[index] = value;
+            }else{
+                printf("Cannot write to memory because it is full!\n");
+            }
+        }
+        void clearIntMemory(int index){
+            if(*intMemory[index] > 0){
+                *intMemory[index] = 0;
+            }else{
+                printf("Cannot clear memory because it is already empty \n");
+            }
+        }
+        void deallocateIntMemory(){
+            for(int i = 0; i < 4; i++){
+                intMemory[i] = nullptr;
+                delete[] intMemory;
+            }
+        }
+        void deallocateDoubleMemory(){
+            for(int i = 0; i < 4; i++){
+                doubleMemory[i] = nullptr;
+                delete[] doubleMemory;
+            }
+        }
+    }
