9be31bb5392c9209eb3e7be4010a79801b84c118
PyDOS8 <124309800+PyDOS8@users.noreply.github.com>
2024-02-08 11:06:08 -0500

    Update CPU.cpp

--- CPU.cpp
+++ CPU.cpp
@@ -6,7 +6,7 @@ extern "C"{
         char *charMemory[1000];
         void writeToStringMemory(string *item){
             for(int i = 0; i < 1000; i++){
-                if(*stringMemory[i] != 0){
+                if(*stringMemory[i] == 0 || *stringMemory[i] > 0 && *stringMemory[i] < 1000){
                      *stringMemory[i] = *item;
                 }else{
                     printf("You cannot write to memory because it is already full");
